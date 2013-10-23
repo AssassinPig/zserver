311f9fb64a951e5f1d72e372c30784b4c108969b
assassinpig <assassinpig@gmail.com>
2013-10-23 11:56:54 -0400

    miss zmutext.hpp

---
+++ zmutex.hpp
@@ -0,0 +1,58 @@
+#ifndef _ZMUTEX_H__
+#define _ZMUTEX_H__
+
+#include "pthread.h"
+
+class ZMutex 
+{
+public:
+	ZMutex() : m_holder(0)
+	{
+		pthread_mutex_init(&m_mutex, NULL);	
+	}
+
+	~ZMutex()
+	{
+		assert(m_holder == 0);
+		pthread_mutex_destroy(&m_mutex);
+	}
+
+	void AssaginHolder()
+	{
+		m_holder = pthread_self();
+	}
+	
+	void Lock()
+	{
+		pthread_mutex_lock(&m_mutex);	
+	}
+
+	void Unlock()
+	{
+		pthread_mutex_unlock(&m_mutex);	
+	}
+
+private:
+	pthread_mutex_t m_mutex;
+	pid_t			m_holder;
+};
+
+
+class ZMutexGuard
+{
+public:
+	ZMutexGuard()
+	{
+		m_mutex.Lock();	
+	}
+
+	~ZMutexGuard()
+	{
+		m_mutex.Unlock();	
+	}
+
+private:
+	ZMutex m_mutex;
+};
+
+#endif //_ZMUTEX_H__
